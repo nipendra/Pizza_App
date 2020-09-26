ae207531e0ba351f74685bdc8050d075611058b9
nipendra <nipendra.dakshana15@gmail.com>
2020-09-26 10:30:32 +0530

    Setup Laravel Mix and use  embeded tailwind css

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ const PORT=process.env.PORT || 3000
 app.get('/',(req,res)=>{
     res.render('home.ejs')
 })  
+// Assets location
+app.use(express.static('public'))
+
 // set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 app.listen(PORT,()=>{
+    console.log(`listing on port ${PORT}`)
 })
+ 
