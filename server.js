a499b40c9b240171a42176cf301f066ae44b81d7
nipendra <nipendra.dakshana15@gmail.com>
2020-11-20 01:03:43 +0530

    cart page added login sign up page added

--- server.js
+++ server.js
@@ -5,16 +5,30 @@ const path=require('path')
 const app=express()
 const PORT=process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-    res.render('home.ejs')
-})  
+
 // Assets location
 app.use(express.static('public'))
 
-// set template engine
+// set template engine  and path to views 
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+// routs
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})  
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart.ejs')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login.ejs')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register.ejs')
+})
 
 app.listen(PORT,()=>{
     console.log(`listing on port ${PORT}`)
